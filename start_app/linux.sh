90d6e4e6f5cb0a132ab73d32c596f3535d80bc67
vuongkienthanh <thanhstardust@outlook.com>
2022-07-11 20:07:54 +0700

    oo

--- start_app/linux.sh
+++ start_app/linux.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 cd $(dirname $(dirname $(realpath $0)))
-poetry run python src/main.py -OO
+poetry run python -OO src/main.py
--- start_app/windows_cmd.bat
+++ start_app/windows_cmd.bat
@@ -1,3 +1,3 @@
 cd %~dp0
 cd ..
-poetry run python src\main.py -OO
+poetry run python -OO src\main.py
